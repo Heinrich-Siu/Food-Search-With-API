9f94c0946dee104d2dcd864b171bc71eb00c0278
Hezz <heinrich@connect.hku.hk>
2020-06-22 18:09:20 +0800

    Initial Commit

---
+++ apiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apiTest
+//
+//  Created by Heinrich Siu on 22/6/2020.
+//  Copyright © 2020 Heinrich Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
